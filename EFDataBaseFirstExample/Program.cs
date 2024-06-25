fc08c780fc90c3ec4a0aa0cb7cf6b2954f5dd154
Emily Morgan <emorgan921@gmail.com>
2024-06-25 09:35:50 -0400

    reformatted code, added remove method at end.

--- EFDataBaseFirstExample/Program.cs
+++ EFDataBaseFirstExample/Program.cs
@@ -5,7 +5,8 @@ internal class Program
     static void Main(string[] args)
     {
         var _context = new TqlprsdbContext(); //creating an instance of the context class
-
+ 
+ //USER//////////////////////////////////////////////////////////////////
         //var newUser = new User
         //{
         //    Id = 0, Username = "km", Password = "km",
@@ -14,8 +15,8 @@ internal class Program
         //    IsReviewer = true, IsAdmin = true
         //};
 
-        ////_context.Users.Add(newUser);
-        ////var rowsAffected = _context.SaveChanges();
+        //_context.Users.Add(newUser);
+        //var rowsAffected = _context.SaveChanges();
 
         //var deleteUser = _context.Users.Find(6);
         //_context.Users.Remove(deleteUser!);
@@ -23,7 +24,7 @@ internal class Program
         //Console.WriteLine($"{rowsAffected} row(s) affected.");
 
 
-
+ //VENDOR////////////////////////////////////////////////////////////////
         //var newVendor = new Vendor
         //{
         //    Id = 0, Code = string.Empty, 
@@ -35,35 +36,35 @@ internal class Program
         //var rowsAffected = _context.SaveChanges();
         //Console.Write($"{rowsAffected} vendor(s) added.");
 
-        //PRODUCT
+ //PRODUCT/////////////////////////////////////////////////////////////////////
 
-        var newProduct = new Product
-        {
-            Id = 0,
-            PartNbr = "45613581686",
-            Name = "Charging Adaptor",
-            Price = 8.99m,
-            Unit = "USD",
-            VendorId = 4
-        };
-        _context.Products.Add(newProduct);
-        var rowsAffected = _context.SaveChanges();
-        Console.Write($"{rowsAffected} products(s) added.");
+        //var newProduct = new Product
+        //{
+        //    Id = 0,
+        //    PartNbr = "45613581686",
+        //    Name = "Charging Adaptor",
+        //    Price = 8.99m,
+        //    Unit = "USD",
+        //    VendorId = 4
+        //};
+        //_context.Products.Add(newProduct);
+        //var rowsAffected = _context.SaveChanges();
+        //Console.Write($"{rowsAffected} products(s) added.");
 
-        //REQUEST
-        /*       var newRequest = new Request
-                     {
-                         Id = 0, Description = "request for more Iphones & new product (charging adaptor).",
-                         Justification = "low stock and new product fufillment",
-                         DeliveryMode = "pickup",
-                         Status = "NEW", Total = 22000.0m, 
-                         UserId = 3
-                     };
-                _context.Requests.Add(newRequest);
-                var rowsAffected = _context.SaveChanges();
-                Console.Write($"{rowsAffected} request(s) added.");     
-     */
-        //TWO REQUESTLINES
+ //REQUEST/////////////////////////////////////////////////////////////////////
+        //var newRequest = new Request
+        //{
+        //    Id = 0, Description = "request for more Iphones & new product (charging adaptor).",
+        //    Justification = "low stock and new product fufillment",
+        //    DeliveryMode = "pickup",
+        //    Status = "NEW", Total = 22000.0m, 
+        //    UserId = 3
+        //};
+        //_context.Requests.Add(newRequest);
+        //var rowsAffected = _context.SaveChanges();
+        //Console.Write($"{rowsAffected} request(s) added.");    
+    
+ //REQUEST LINES/////////////////////////////////////////////////////////////
         //var newRL = new Requestline
         //{
         //    Id = 0, RequestId= 2, ProductId = 9,
@@ -78,15 +79,15 @@ internal class Program
         //};
 
         //_context.Requestlines.Add(newRL2);
-
-
-
-
        // _context.SaveChanges();
 
-
-
-
+//REMOVE///////////////////////////////////////////////////////////////////
+    
+        var removeProduct = _context.Products.Find(17);
+        {
+            _context.Products.Remove(removeProduct!);
+            _context.SaveChanges();
+        };
     }
     }
      
